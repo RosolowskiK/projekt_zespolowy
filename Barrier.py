f3021f449b1ee3c801eb0822e597e4f41fae7421
szywski27@gmail.com <LOki6262>
2021-06-26 23:52:03 +0200

    update barrier - add is_open bool method

--- Barrier.py
+++ Barrier.py
@@ -1,12 +1,17 @@
 class Barrier:
+    open = False;
 
     def __init__(self):
         pass
 
     def open(self):
+        self.open = True;
         print("The barrier opens")
 
     def close(self):
+        self.open = False;
         print("The barrier closes")
 
+    def is_open(self):
+        return self.open
 
